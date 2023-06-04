5187401339ca97ea7d1fe0d2415f2b95ccae2825
John Krone <john.krone@kellogg.northwestern.edu>
2023-06-04 16:31:21 +0000

    layout changes

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,14 +1,18 @@
 class PlacesController < ApplicationController
-
+  before_action :check_user, except: [:index]
   def index
     @places = Place.all
   end
 
+    
+ 
   def show
     @place = Place.find_by({ "id" => params["id"] })
     @posts = Post.where({ "place_id" => @place["id"], "user_id" => @current_user["id"]})
   end
-  #  authorization "user_id" => @current_user["id"]
+  
+
+
   def new
     @place = Place.new
   end
@@ -18,6 +22,13 @@ class PlacesController < ApplicationController
     @place["name"] = params["place"]["name"]
     @place.save
     redirect_to "/places"
+  end
+
+
+  private
+
+  def check_user
+    redirect_to "/" unless @current_user || request.path == "/"
   end
 
 end
